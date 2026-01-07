cf3672acce9cf4cea9cd80d4a50a2de003144971
Oliver GD <114223604+OGD311@users.noreply.github.com>
2026-01-07 16:19:40 +0000

    Fixed light/dark scheme styling.

--- src/components/SectionPopout.tsx
+++ src/components/SectionPopout.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useState } from "react"
+import { IoIosArrowUp } from "react-icons/io";
 import { MdClose, MdTableRows } from "react-icons/md";
 
 export default function SectionPopout({ sections } : { sections: string[] }) {
@@ -8,20 +9,16 @@ export default function SectionPopout({ sections } : { sections: string[] }) {
 
     return (
         <>
-            <div className={`${isOut ? '' : 'hidden'} text-right text-3xl fixed bottom-20 right-8 flex flex-col z-50 backdrop-blur-sm rounded-xl px-2 py-1 border border-white`}>
+            <div className={`${isOut ? 'opacity-100 translate-y-0' : 'opacity-0 pointer-events-none translate-y-60'} text-foreground border-foreground transition-all duration-300 text-right text-3xl fixed bottom-20 right-8 flex flex-col z-50 backdrop-blur-md rounded-xl px-5 py-3 border`}>
                 {sections.map((section, idx) => (
-                    <Link href={`#${section}`} key={idx}>{section.slice(0,1).toUpperCase()}{section.slice(1)}</Link>
+                    <Link href={`#${section}`} key={idx} className="hover:underline">{section.slice(0,1).toUpperCase()}{section.slice(1)}</Link>
                 ))}
             </div>
             <button 
                 onClick={() => setIsOut(!isOut)}
-                className="cursor-pointer fixed bottom-3 right-8 bg-black p-3 rounded-2xl z-50 border border-white"
+                className="cursor-pointer fixed bottom-3 right-8 p-3 rounded-2xl z-50 border text-foreground bg-background border-foreground"
                 >
-                {isOut ? (
-                    <MdClose className="w-8 h-8" />
-                ) : (
-                    <MdTableRows className="w-8 h-8" />
-                )}
+                <IoIosArrowUp className={`w-8 h-8 ${isOut ? 'rotate-0' : '-rotate-180'} transition-transform duration-200`} />
             </button>
         </>
     )
