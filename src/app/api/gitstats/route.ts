a04b7aeb62a183c7332435cde574c44edee4388e
Oliver GD <114223604+OGD311@users.noreply.github.com>
2026-01-07 00:12:50 +0000

    Updated graphql query to include main commit count.

--- src/app/api/gitstats/route.ts
+++ src/app/api/gitstats/route.ts
@@ -1,29 +1,40 @@
 export async function GET() {
   const query = `
-    query {
-      user(login: "OGD311") {
-          repositories(first: 10, orderBy: {field: CREATED_AT, direction: DESC}) {
-              nodes {
-                  name
-                  description
-                  url
-                  stargazerCount
-                  forkCount
-                  updatedAt
-                  pushedAt
-                  languages(first: 10, orderBy: { field: SIZE, direction: DESC }) {
-                      totalSize
-                      edges {
-                          size
-                          node {
-                              name
-                          }
-                      }
-                  }
-              }
-          }
-      }
-  }`
+        query {
+        user(login: "OGD311") {
+            repositories(first: 10, orderBy: {field: CREATED_AT, direction: DESC}) {
+                nodes {
+                    name
+                    description
+                    url
+                    stargazerCount
+                    forkCount
+                    updatedAt
+                    pushedAt
+                    languages(first: 10, orderBy: { field: SIZE, direction: DESC }) {
+                        totalSize
+                        edges {
+                            size
+                            node {
+                                name
+                            }
+                        }
+                    }
+                    defaultBranchRef {
+                        name
+                        target {
+                            ... on Commit {
+                                history {
+                                    totalCount
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
 
   const result = await fetch("https://api.github.com/graphql", {
     method: "POST",
--- src/scripts/fetchgitstats.js
+++ src/scripts/fetchgitstats.js
@@ -2,30 +2,41 @@ import { writeFile } from "fs/promises";
 import "dotenv/config";
 
 const query = `
-  query {
-    user(login: "OGD311") {
-        repositories(first: 10, orderBy: {field: CREATED_AT, direction: DESC}) {
-            nodes {
-                name
-                description
-                url
-                stargazerCount
-                forkCount
-                updatedAt
-                pushedAt
-                languages(first: 10, orderBy: { field: SIZE, direction: DESC }) {
-                    totalSize
-                    edges {
-                        size
-                        node {
-                            name
+        query {
+        user(login: "OGD311") {
+            repositories(first: 10, orderBy: {field: CREATED_AT, direction: DESC}) {
+                nodes {
+                    name
+                    description
+                    url
+                    stargazerCount
+                    forkCount
+                    updatedAt
+                    pushedAt
+                    languages(first: 10, orderBy: { field: SIZE, direction: DESC }) {
+                        totalSize
+                        edges {
+                            size
+                            node {
+                                name
+                            }
+                        }
+                    }
+                    defaultBranchRef {
+                        name
+                        target {
+                            ... on Commit {
+                                history {
+                                    totalCount
+                                }
+                            }
                         }
                     }
                 }
             }
         }
     }
-}`
+`
 
 const result = await fetch("https://api.github.com/graphql", {
   method: "POST",
